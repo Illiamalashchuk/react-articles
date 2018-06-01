056c71eac9c4a945c3687189c8397c0d268cc6fe
Illia Malashchuk <IMalashchuk@on.seo>
2018-06-01 18:02:42 +0300

    accordeon has already added

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,33 @@
-import React from 'react'
+import React, {PureComponent} from 'react'
 import ArticleList from './ArticleList'
 import articles from '../fixtures'
+import 'bootstrap/dist/css/bootstrap.css'
 
-function App() {
-    return (
-        <div>
-            <h1>App name</h1>
-            <ArticleList articles={articles} />
-        </div>
-    )
+
+class App extends PureComponent {
+    constructor(props) {
+        super(props)
+        this.state = {
+            reverted: false
+        }
+    }
+    render() {
+        return (
+            <div className="container">
+                <div className="jumbotron">
+                    <h1>App name</h1>
+                    <button className="btn" onClick={this.revert}>Revert</button>
+                </div>
+                <ArticleList articles={this.state.reverted ? articles.slice().reverse() : articles} />
+            </div>
+        )
+    }
+    revert = () => {
+        this.setState({
+            reverted: !this.state.reverted
+        })
+    }
 }
+
 
 export default App
--- src/components/Article.js
+++ src/components/Article.js
@@ -1,38 +1,60 @@
-import React, {Component} from 'react'
+import React, {PureComponent} from 'react'
 
 
-class Article extends Component {
-    // constructior(props) {
-    //     super(props)
-    //     this.state = {
-    //         isOpen: true
-    //     }
+class Article extends PureComponent {
+    constructor(props) {
+        super(props)
+        this.state = {
+            count: 0
+        }
+    }
+
+    // shouldComponentUpdate(nextProps, nextState) {
+    //     return this.state.isOpen !== nextState.isOpen
     // }
-    state = {
-        isOpen: true
+
+    componentWillMount() {
+        console.log('mounting')
+    }
+
+    // componentWillReceiveProps(nextProps) {
+    //     if(nextProps.defaultOpen !== this.props.defaultOpen) this.setState({
+    //         isOpen: nextProps.defaultOpen
+    //     })
+    // }
+
+    componentWillUpdate() {
+        console.log('will update')
     }
     render() {
-        const {article} = this.props
-        const body = this.state.isOpen && <section>{article.text}</section>
+        const {article, isOpen, onButtonClick} = this.props
+        const body = isOpen && <section className="card-text">{article.text}</section>
         return (
-            <div className="hello">
-                <h2>
-                    {article.title}
-                    <button onClick={this.handleClick}>
-                        {this.state.isOpen ? 'close': 'open'} {/* if 'this.state.isOpen' = true => 'close' */}
-                    </button>
-                </h2>
-                {body}
-                <h3>creation date: {(new Date(article.date)).toDateString()}</h3>
+            <div className="card mx-auto" style={{width: '70%'}}>
+                <div className="card-header">
+                    <h2 onClick={this.incrementCounter}>
+                        {article.title}
+                        clicked {this.state.count}
+                        <button onClick={onButtonClick} className="btn btn-primary btn-lg float-right">
+                            {isOpen ? 'close': 'open'} {/* if 'this.state.isOpen' = true => 'close' */}
+                        </button>
+                    </h2>
+                </div>
+                <div className="card-body">
+                    <h6 className="card-subtitle text-muted">
+                        creation date: {(new Date(article.date)).toDateString()}
+                    </h6>
+                    {body}
+                </div>
             </div> 
         )
     }
-
-    handleClick = () => {
+    incrementCounter = () => {
         this.setState({
-            isOpen: !this.state.isOpen
-        }) 
+            count: this.state.count + 1
+        })
     }
+    
 }
 
 
